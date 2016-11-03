c30a03a285c571d1e774a6903a20c42594a3bc73
javohotel <ja.ramirez@live.cl>
2016-11-03 19:00:20 -0400

    ohhhsu

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var sc1 = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3328.931221801205!2d-70.6404791848007!3d-33.45109838077376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c5716c4f3791%3A0xd720b483e1bb69d4!2sLira+650%2C+Santiago%2C+Regi%C3%B3n+Metropolitana!5e0!3m2!1ses!2scl!4v1478201622188"  height="600" frameborder="0" style="width: 100%;border:0;" allowfullscreen></iframe>';
+var sc2 = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3328.8545461110652!2d-70.63988128518314!3d-33.453095880773226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c571bf2e6e7b%3A0x5e93ffe992332316!2sLira+754%2C+Santiago%2C+Regi%C3%B3n+Metropolitana!5e0!3m2!1ses!2scl!4v1478212460574"  height="600" frameborder="0" style="width: 100%;border:0;" allowfullscreen></iframe>';
+var sc3 = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3328.945013399627!2d-70.64012538518317!3d-33.45073908077388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c571151fda33%3A0x88c16904fd617d09!2sGral+Urriola+628%2C+Santiago%2C+Regi%C3%B3n+Metropolitana!5e0!3m2!1ses!2scl!4v1478212524226"  height="600" frameborder="0" style="width: 100%;border:0;" allowfullscreen></iframe>';
+var sc4 = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.376333001244!2d-70.62562368518353!3d-33.43950058077703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c58045408c57%3A0xeccd15ca7e00fd2b!2sRancagua+756%2C+Providencia%2C+Regi%C3%B3n+Metropolitana!5e0!3m2!1ses!2scl!4v1478212561923"  height="600" frameborder="0" style="width: 100%;border:0;" allowfullscreen></iframe>';
+
+
+function setAdressA(){
+		document.getElementById('map').innerHTML = sc1;
+	}
+
+function setAdressB(){
+		document.getElementById('map').innerHTML = sc2;
+	}
+
+function setAdressC(){
+		document.getElementById('map').innerHTML = sc3;
+	}
+
+function setAdressD(){
+		document.getElementById('map').innerHTML = sc4;
+	}
